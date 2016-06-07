738d604f72c8fb4c927c1a8d51a7b2ae8dcc7eaa
lbel <l.bel@cern.ch>
2016-06-07 15:15:10 +0200

    Traverse internal TFile structure

--- common.py
+++ common.py
@@ -5,12 +5,18 @@ files = [ROOT.TFile.Open(arg) for arg in sys.argv[1:]]
 
 def perform_trees(fn):
     for f in files:
-        for key in f.GetListOfKeys():
-            if key.GetClassName() != "TTree":
-                continue
-        tree_name = key.GetName()
-        tree = f.Get(tree_name)
-        if tree and isinstance(tree, ROOT.TTree):
-            print("{}/{}".format(f.GetName(), tree_name))
-            fn(tree)
+        __perform_trees_on_file(fn, f)
 
+def __perform_trees_on_file(fn, f):
+    for key in f.GetListOfKeys():
+        if key.GetClassName() == "TDirectoryFile":
+            __perform_trees_on_file(fn, f.Get(key.GetName()))
+        elif key.GetClassName() == "TTree":
+            tree_name = key.GetName()
+            tree = f.Get(tree_name)
+            if tree and isinstance(tree, ROOT.TTree):
+                print("{}/{}".format(f.GetName(), tree_name))
+                fn(tree)
+        else:
+            print("Not a TTree {}".format(key.GetName()))
+            continue
